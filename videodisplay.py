6ec887e6f6c57848ecc672b9d10dd4c09e915c81
evan <117702363+evankroz@users.noreply.github.com>
2024-03-15 11:55:27 -0400

    vid display

---
+++ videodisplay.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+def play_video(file_path):
+
+    cap = cv.VideoCapture(file_path)
+
+    while cap.isopened():
+        ret, frame = cap.read()
+        frame = cv.resize(frame, fx = 0.5, fy = 0.5)
+
+        while ret == True:
+            cv.imshow(frame)
+
+            if cv.waitKey(25) & 0xFF == ord("q"):
+                break
+        else:
+            break
+cap.release()
+cap.destroyAllWindows()
+if __name__ == "__main__":
+    runvid("highway.mp4")
